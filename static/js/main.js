db1ec566a06e35fa22f0936bbd446220b8817d37
Jefferson Fernandes <suportefinanceiro@rcostaadv.com.br>
2026-01-12 18:33:45 -0300

    Reset de migracões com novos campos

--- static/js/main.js
+++ static/js/main.js
@@ -7,3 +7,44 @@ setTimeout(function() {
         alert.style.display = 'none';
     });
 }, 5000);
+
+const mayaModal = {
+  el: document.getElementById('maya-modal'),
+
+  open({ title, subtitle, url }) {
+    document.getElementById('modal-title').innerText = title;
+    document.getElementById('modal-subtitle').innerText = subtitle || '';
+
+    fetch(url)
+      .then(res => res.text())
+      .then(html => {
+        document.getElementById('modal-content').innerHTML = html;
+        this.el.showModal();
+      });
+  },
+
+  close() {
+    this.el.close();
+    document.getElementById('modal-content').innerHTML = '';
+  }
+};
+
+<script>
+function openCheckinModal(data) {
+  const modal = document.getElementById('modal_checkin')
+
+  document.getElementById('modalAluno').innerText = data.aluno
+  document.getElementById('modalData').innerText = data.data
+  document.getElementById('modalOcupacao').innerText = `Ocupação: ${data.ocupacao}`
+
+  const icon = document.getElementById('modalIcon')
+  icon.style.backgroundColor = data.cor + '20'
+  icon.style.color = data.cor
+
+  const form = document.getElementById('checkinForm')
+  form.action = data.action
+  form.reset()
+
+  modal.showModal()
+}
+</script>
